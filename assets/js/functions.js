768d8caaed2f3c9d3d8e5b763e94fa26bbe99cb3
m8524769 <m8524769@163.com>
2017-09-10 23:36:31 +0800

    Feat: 网页随机背景音乐

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,9 @@
-window.onload = function changeDescriptionByClick() {
+window.onload = function initiation() {
+    changeDescriptionByClick();
+    randomMusic();
+}
+
+function changeDescriptionByClick() {
     var descriptions = [
         "How are you today?",
         "My name is Wang Lei.",
@@ -8,6 +13,7 @@ window.onload = function changeDescriptionByClick() {
         "I wish you like it.",
         "Anyway..",
         "Have a good time, bro!",
+        "All the best."
     ];
     var i = 0;
     var description = document.getElementById("description");
@@ -15,4 +21,23 @@ window.onload = function changeDescriptionByClick() {
         if (i == descriptions.length) i = 0;
         description.innerHTML = descriptions[i++];
     });
+}
+
+function randomMusic() {
+    var songID = [
+        "418602088", // Nevada
+        "420069116", // Anywhere I Go
+        "493551046", // Collide
+        "485347980", // I Hear You
+        "404783169", // Bright Side
+        "427542254", // Kaleidoscope
+        "419594258", // Hymn For The Weekend [Remix]
+        "27929366",  // Shotgun (feat. Rochelle)
+        "461518837", // Good Day
+        "29732234",  // Till It Hurts (Radio Edit)
+        "440208433", // Love & War (Original Mix)
+    ];
+    var srcPrefix = "//music.163.com/outchain/player?type=2&auto=1&height=66&id=";
+    var randomNum = Math.floor(Math.random() * songID.length);
+    document.getElementById("neteaseMusic").src = srcPrefix.concat(songID[randomNum]);
 }
