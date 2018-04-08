beb68812dd55383bddbf1a39993015015f761f56
m8524769 <m8524769@163.com>
2018-04-08 18:00:40 +0800

    Feature: Random background picture(Not finished)
    - Remove random music feature

--- assets/js/func.js
+++ assets/js/func.js
@@ -1,51 +1,37 @@
 window.onload = () => {
-    changeInnerByClick();
-    randomMusic();
+    randomPicture()
+    updateDescriptionByClick()
 }
 
-const expandPost = () => {
-    let post = document.getElementById("post");
-    post.addEventListener("click", () => {
-    });
+const randomPicture = () => {
+    let srcPrefix = "https://source.unsplash.com/random/"
+    let resolution = `${screen.width}x${screen.height}`
+    document.getElementById("bg").src = srcPrefix.concat(resolution)
+
+    caches.open('bg-next').then(cache => {
+        cache.add(srcPrefix.concat(resolution))
+    })
+
+    localStorage["visited"] = true
 }
 
-const changeInnerByClick = () => {
+const updateDescriptionByClick = () => {
     let inners = [
-        "How are you today?",
-        // "My name is Wang Lei.",
-        // "And yk is the nickname.",
-        "Here is my GitHub Page,",
-        "which is only a demo.",
-        "I wish you like it.",
+        "Here is my personal blog",
         "Anyway..",
-        "Have a good time!",
-        "All the best."
-    ];
-    let i = 0;
-    let inner = document.getElementById("inner");
-    inner.addEventListener("click", () => {
-        if (i < inners.length)
-            inner.innerHTML = inners[i++];
-    });
-}
-
-const randomMusic = () => {
-    let songID = [
-        "418602088", // Nevada
-        "420069116", // Anywhere I Go
-        "493551046", // Collide
-        "512359300", // New Rules
-        "404783169", // Bright Side
-        "427542254", // Kaleidoscope
-        "435592093", // Starboy
-        "417833052", // I Want It All
-        "419594258", // Hymn For The Weekend [Remix]
-        "428350510", // Shanghai
-        "409031839", // I Took A Pill In Ibiza
-        "29761105",  // Downtown
-        "30064291",  // Green Sky
-    ];
-    let srcPrefix = "//music.163.com/outchain/player?type=2&auto=0&height=66&id=";
-    let randomNum = Math.floor(Math.random() * songID.length);
-    document.getElementById("neteaseMusic").src = srcPrefix.concat(songID[randomNum]);
+        "Have a good time!!",
+    ]
+    let description = document.getElementById("description")
+    let i = 0
+    description.addEventListener("click", () => {
+        if (i < inners.length) {
+            description.innerHTML = inners[i++]
+        }
+        if (i == inners.length) {
+            let bg = document.getElementById("bg")
+            bg.style.animation = "fadein 4s"
+            bg.style.opacity = 1
+            document.getElementById("article").classList.add('light')
+        }
+    })
 }
