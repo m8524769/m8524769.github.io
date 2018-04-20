1a539f87c11a2d4632388de2f3ac2fa9c01bf94d
m8524769 <m8524769@163.com>
2018-04-21 00:55:13 +0800

    Style: Change the indentation

--- assets/js/func.js
+++ assets/js/func.js
@@ -1,68 +1,68 @@
 const randomBackground = () => {
-    let bgImg = document.getElementById('bg')
-    let baseURL = 'https://source.unsplash.com/random/'
-    let resolution = `${screen.width}x${screen.height}`
-    let imageURL = baseURL.concat(resolution)
+  let bgImg = document.getElementById('bg')
+  let baseURL = 'https://source.unsplash.com/random/'
+  let resolution = `${screen.width}x${screen.height}`
+  let imageURL = baseURL.concat(resolution)
 
-    caches.match(imageURL).then(response => {
-        bgImg.src = response.url
-    }).catch(() => {
-        fetch(imageURL).then(response => {
-            if (response.ok) {
-                bgImg.src = response.url
-                caches.open('bg-next').then(cache => {
-                    cache.put(imageURL, response)
-                })
-            }
-        })
-    })
-
+  caches.match(imageURL).then(response => {
+    bgImg.src = response.url
+  }).catch(() => {
     fetch(imageURL).then(response => {
+      if (response.ok) {
+        bgImg.src = response.url
         caches.open('bg-next').then(cache => {
-            cache.put(imageURL, response)
+          cache.put(imageURL, response)
         })
+      }
     })
+  })
+
+  fetch(imageURL).then(response => {
+    caches.open('bg-next').then(cache => {
+      cache.put(imageURL, response)
+    })
+  })
 }
 
 window.DesktopInit = () => {
-    randomBackground()
-    let inners = [
-        "It's my personal blog",
-        "Anyway..",
-        "Have a good time!!",
-    ]
-    let greeting = document.getElementById("greeting")
-    greeting.style.transition = "color 1s"
-    let i = 0
-    greeting.addEventListener("click", () => {
-        if (i < inners.length) {
-            greeting.innerHTML = inners[i++]
-        }
-        if (i == inners.length) {
-            let bg = document.getElementById("bg")
-            bg.style.animation = "fadein 4s"
-            bg.style.opacity = 1
-            document.getElementById("article").classList.add('light')
-            greeting.style.color = "#eeeeee"
-            greeting.style.transition = "opacity 1.5s"
-            greeting.style.opacity = 0
-        }
-    })
+  randomBackground()
+  let inners = [
+    "It's my personal blog",
+    "Anyway..",
+    "Have a good time!!",
+  ]
+  let greeting = document.getElementById("greeting")
+  greeting.style.transition = "color 1s"
+  let i = 0
+  greeting.addEventListener("click", () => {
+    if (i < inners.length) {
+      greeting.innerHTML = inners[i++]
+    }
+    if (i == inners.length) {
+      let bg = document.getElementById("bg")
+      bg.style.animation = "fadein 4s"
+      bg.style.opacity = 1
+      document.getElementById("article").classList.add('light')
+      greeting.style.color = "#eeeeee"
+      greeting.style.transition = "opacity 1.5s"
+      greeting.style.opacity = 0
+    }
+  })
 }
 
 if (screen.width > 480) {
-    window.DesktopInit()
+  window.DesktopInit()
 }
 
 // Register Service Worker
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(registration => {
-            console.log('ServiceWorker registration successful: ', registration)
-        }).catch(err => {
-            console.log('ServiceWorker registration failed: ', err)
-        })
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js').then(registration => {
+      console.log('ServiceWorker registration successful: ', registration)
+    }).catch(error => {
+      console.log('ServiceWorker registration failed: ', error)
     })
+  })
 }
 
 localStorage['visited'] = true
--- sw.js
+++ sw.js
@@ -1,50 +1,50 @@
 let CACHE_NAME = 'yk-blog'
 
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll([
-                '/',
-                '/assets/css/main.css',
-                '/assets/js/func.js',
-                '/assets/js/particles.js'
-            ])
-        })
-    )
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/',
+        '/assets/css/main.css',
+        '/assets/js/func.js',
+        '/assets/js/particles.js'
+      ])
+    })
+  )
 })
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            if (response && response.type == 'basic') {
-                return response
-            }
-            return fetch(event.request).then(response => {
-                if (response.type == 'basic') {
-                    return caches.open(CACHE_NAME).then(cache => {
-                        cache.put(event.request, response.clone())
-                        return response
-                    })
-                } else {
-                    return response
-                }
-            })
-        })
-    )
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response && response.type == 'basic') {
+        return response
+      }
+      return fetch(event.request).then(response => {
+        if (response.type == 'basic') {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, response.clone())
+            return response
+          })
+        } else {
+          return response
+        }
+      })
+    })
+  )
 })
 
 self.addEventListener('activate', event => {
-    return event.waitUntil(
-        caches.keys().then(keys => {
-            return Promise.all(
-                keys.map(k => {
-                    if (k == CACHE_NAME) {
-                        return caches.delete(k)
-                    } else {
-                        return Promise.resolve()
-                    }
-                })
-            )
+  return event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(k => {
+          if (k == CACHE_NAME) {
+            return caches.delete(k)
+          } else {
+            return Promise.resolve()
+          }
         })
-    )
+      )
+    })
+  )
 })
