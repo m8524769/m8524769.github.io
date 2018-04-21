923c2d23093f071c6e092867b8ac3e9cee855d97
m8524769 <m8524769@163.com>
2018-04-21 20:14:04 +0800

    Feature: Vim-like keybindings to control the web!!! (Not finished yet...)

--- assets/js/func.js
+++ assets/js/func.js
@@ -24,8 +24,73 @@ const randomBackground = () => {
   })
 }
 
-window.DesktopInit = () => {
+const vimLike = () => {
+  let home = document.getElementById('logo')
+  if (window.location.pathname == '/') { // In post list
+    let sections = document.getElementsByName('focusable')
+    let i = -1
+    window.addEventListener('keypress', event => {
+      if (event.key == 'j' && i < sections.length - 1) {
+        sections[++i].focus()
+      } else if (event.key == 'k') {
+        if (i == -1 || i == 0) {
+          i = 1
+        }
+        sections[--i].focus()
+      } else if (event.key == 'G') {
+        i = sections.length - 1
+        sections[i].focus()
+      } else if (event.key == 'o') {
+        sections[i].click()
+      } else if (event.key == 'u') {
+        home.click()
+      }
+    })
+  } else { // In article
+    let article = document.getElementById('article')
+    window.addEventListener('keypress', event => {
+      if (event.key == 'j') {
+        scrollBy(0, 27)
+      } else if (event.key == 'k') {
+        scrollBy(0, -27)
+      } else if (event.key == 'J') {
+        scrollBy({
+          top: 240,
+          behavior: "smooth"
+        })
+      } else if (event.key == 'K') {
+        scrollBy({
+          top: -240,
+          behavior: "smooth"
+        })
+      } else if (event.key == 'G') {
+        scroll({
+          top: article.offsetHeight,
+          behavior: "smooth"
+        })
+      } else if (event.key == 'u') {
+        home.click()
+      }
+    })
+  }
+}
+
+const lightUp = () => {
+  let bg = document.getElementById("bg")
+  bg.style.animation = "fadein 4s"
+  bg.style.opacity = 1
+  let article = document.getElementById('article')
+  if (article) {
+    article.classList.add('light')
+  }
+  greeting.style.color = "#eeeeee"
+  greeting.style.transition = "opacity 1.5s"
+  greeting.style.opacity = 0
+}
+
+window.desktopInit = () => {
   randomBackground()
+  vimLike()
   let inners = [
     "It's my personal blog",
     "Anyway..",
@@ -34,35 +99,32 @@ window.DesktopInit = () => {
   let greeting = document.getElementById("greeting")
   greeting.style.transition = "color 1s"
   let i = 0
-  greeting.addEventListener("click", () => {
-    if (i < inners.length) {
-      greeting.innerHTML = inners[i++]
-    }
-    if (i == inners.length) {
-      let bg = document.getElementById("bg")
-      bg.style.animation = "fadein 4s"
-      bg.style.opacity = 1
-      document.getElementById("article").classList.add('light')
-      greeting.style.color = "#eeeeee"
-      greeting.style.transition = "opacity 1.5s"
-      greeting.style.opacity = 0
-    }
-  })
+  if (sessionStorage.getItem('light')) {
+    lightUp()
+  } else {
+    greeting.addEventListener("click", () => {
+      if (i < inners.length) {
+        greeting.innerHTML = inners[i++]
+      }
+      if (i == inners.length) {
+        lightUp()
+        sessionStorage.setItem('light', true)
+      }
+    })
+  }
 }
 
 if (screen.width > 480) {
-  window.DesktopInit()
+  window.desktopInit()
 }
 
 // Register Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').then(registration => {
-      console.log('ServiceWorker registration successful: ', registration)
+      console.log('ServiceWorker registration successful.')
     }).catch(error => {
       console.log('ServiceWorker registration failed: ', error)
     })
   })
 }
-
-localStorage['visited'] = true
